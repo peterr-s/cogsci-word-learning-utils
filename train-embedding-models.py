e6981bc06f3a0b7a4a6fa166f8451cd076c6255b
Peter Schoener <peter.c.schoener@gmail.com>
2022-12-18 22:36:10 -0800

    use NLTK dataloader for CHILDES

--- train-embedding-models.py
+++ train-embedding-models.py
@@ -1,17 +1,32 @@
+#!/usr/bin/env python3
+
 import sys
+import os
 import logging
 import argparse
 
-from gensim.models import Word2Vec, FastText, Poincare
+import nltk
+from nltk.corpus.reader import CHILDESCorpusReader
+
+from gensim.models import Word2Vec, FastText #, Poincare
 from gensim.corpora.textcorpus import TextCorpus
 
 logger = logging.getLogger(__name__)
 
 class CHILDESCorpus(TextCorpus) :
-    pass
+    def __init__(self) :
+        childes_root = nltk.data.find("corpora/childes/data-xml/Eng-USA-MOR/")
+        self.reader = CHILDESCorpusReader(childes_root, ".*/.*.xml")
+
+    def get_texts(self) :
+        return self.reader.sents(self.reader.fileids(), speaker = ["CHI"])
+        #for fileid in self.reader.fileids() :
+        #    for sent in self.reader.sents(fileid, speaker = ["CHI"]) :
+        #        yield sent
 
 def main(corpus_path, model_types) :
     logger.info("loading CHILDES")
+    corpus = CHILDESCorpus()
 
     # create and train models on provided data
     for model_type in model_types :
@@ -19,13 +34,14 @@ def main(corpus_path, model_types) :
 
         model = None
         if model_type == "word2vec" :
-            model = Word2Vec(sentences)
+            model = Word2Vec(corpus.get_texts())
         elif model_type == "fasttext" :
-            model = FastText(sentences)
-        elif model_type == "poincare" :
-            model = Poincare(sentences)
+            model = FastText(corpus.get_texts())
+        #elif model_type == "poincare" :
+        #    model = Poincare(corpus.get_texts())
 
         logger.info("saving model")
+        model.save("./model_out")
         
         logger.info("saved model")
 
@@ -41,11 +57,11 @@ if __name__ == "__main__" :
             )
     parser.add_argument(
             "-m", "--model-type",
-            action = append,
+            action = "append",
             choices = ["word2vec", "fasttext", "poincare"],
             dest = "model_types",
             help = "the type of model to train",
             required = True
             )
 
-    main(**parser.parse_args(sys.argv[1:]))
+    main(**vars(parser.parse_args(sys.argv[1:])))
